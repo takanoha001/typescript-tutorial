4eb868e603e1094a85aa167a7369808dc880564d
takanoha001 <72856896+takanoha001@users.noreply.github.com>
2021-03-20 15:22:45 +0900

    adding Test.tsx and updated index.tsx

---
+++ react-tutorial-typescript/src/Test.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+ 
+
+function Test() {
+  return (
+    <div className="Test">
+      <header className="Test-header">
+    <p>
+      Testing...
+    </p>
+      </header>
+    </div>
+  );
+}
+
+export default Test;

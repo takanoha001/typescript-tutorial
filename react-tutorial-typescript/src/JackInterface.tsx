b48e843b5ac9ed3927f85d91f46e3d924d834e3f
takanoha001 <72856896+takanoha001@users.noreply.github.com>
2021-03-25 00:42:03 +0900

    added updated functions and their tests. todo: redo the mechanism to start jadckd and jacktrip and kill

--- react-tutorial-typescript/src/JackInterface.tsx
+++ react-tutorial-typescript/src/JackInterface.tsx
@@ -1,5 +1,6 @@
 import { spawnSync, spawn } from "child_process";
 import { notDeepEqual } from "node:assert";
+import { clear } from "node:console";
 import { pathToFileURL } from "url";
 
 const kill = require ('tree-kill');
@@ -12,7 +13,7 @@ console.log('------ start program ------ ');
 features:
 - search the jacktrip and jackd path and store in the path variable
 - output the log to some kind of buffer 
-- programatically find out if jacktrip is running or not
+
 
 functions:
 x- start jacklsp (programatically find out if jackd is running or not with jacklsp)
@@ -20,6 +21,12 @@ x- check version info for jackd and jacktrip
 x- start jackdmp 
 x- start jacktrip
 - killing mechanism to kill all the offspinrg with "tree-kill". store pids used for jackd and jacktrip 
+  use killall and use timeout and hardcode 3 seconds 
+x- programatically find out if jacktrip is running or not. we can use lsp and find if its output contains Jacktrip
+
+
+redo the mechanism to start jackd and jacktrip....
+
 
 
  */
@@ -124,7 +131,7 @@ drwxr-xr-x  11 Dicekay  staff    374 17 Mar 20:59 ../
 
 
 
-let SleepTimeoutKill = (sleepInMs:number, pid:number) => {
+export let SleepTimeoutKill = (sleepInMs:number, pid:number) => {
 
   setTimeout(function () {
     console.log(pid +'=== SleepTimeoutKill -- x');
@@ -186,7 +193,7 @@ const paramsToString = (params: string[]) => {
  * 
  * returns jackd version
  */
-let JackdVersion = () =>{
+export let JackdVersion = () =>{
   let cmd = "jackd";
   let params = ["-v"];
 
@@ -204,7 +211,7 @@ let JackdVersion = () =>{
  * 
  * returns jacktrip version
  */
-let JacktripVersion = () =>{
+export let JacktripVersion = () =>{
   let cmd = "jacktrip";
   let params = ["-v"];
 
@@ -237,7 +244,7 @@ jackd -d coreaudio -p 2 -p 256 -r 48000
 
  * 
  */
-let JackdStart = () =>{
+export let JackdStart = () =>{
   const cmd = "jackd";
   const params = ['-d', 'coreaudio', '-p', '2', '-p', '256', '-r', '48000',];
 
@@ -273,7 +280,7 @@ console.log("command: "+ spawnProcess.command);
 let lala = SleepAndKill(spawnProcess.pid, 10000);
  * 
  */
-let JackTripStart = () =>{
+export let JackTripStart = () =>{
   const cmd = "jacktrip";
   const params = ['-n', '1', '-z', '-q', '4', '-b', '16', '-r', '1', '-C', '66.42.72.238'];
 
@@ -290,18 +297,57 @@ let JackTripStart = () =>{
 }
 
 /**
+ * IsJacktripRunning
+ * 
+ * check if jacktrip is running successfully or not
+ * todo combine to IsJackServerRunning()
+ * 
+ * eg. 
+ * 
+ * if Jacktrip is succesfully running and calling jack_lsp will output:
+ * 
+ * system:capture_1
+ * system:capture_2
+ * system:playback_1 
+ * system:playback_2
+ * JackTrip:send_1
+ * JackTrip:receive_1
+ * 
+ * search "JackTrip"
+ */
+export const IsJacktripRunning = () => {
+  // jack_lsp will return an error if jack server isn't running
+  const cmd = "jack_lsp";
+  const proc = spawnSync(cmd);
+
+  console.log("===IsJackServerRunning = " + proc.stdout.toString());
+  console.log("===proc.stats = " + proc.status);
+
+  return (proc.status === 0)&&(proc.stdout.toString().includes('JackTrip'));
+};
+
+
+/**
  * IsJackServerRunning
  * 
- * check if jackd is running successfully or not
+ * eg. 
  * 
+ * if jackd is successfully running proc.status is equal to 0 
+ * otherwise 1
  */
 export const IsJackServerRunning = () => {
-  // jack_lsp will return an error if jack server isn't running
   const cmd = "jack_lsp";
   const proc = spawnSync(cmd);
   return proc.status === 0;
 };
 
+
+export const killProcesses = () => {
+  spawnSync('killall', ['jacktrip']);
+  spawnSync('killall', ['jack_connect']);
+  spawnSync('killall', ['jackdmp']);
+  spawnSync('killall', ['jackd']);
+};
 
 
 
@@ -320,6 +366,7 @@ export const IsJackServerRunning = () => {
  */
 function main2 (){
 
+
   let jackd_pid = 99;
   let jacktrip_pid = 99;
 
@@ -328,23 +375,42 @@ function main2 (){
   let spawnProcess1 = JackdStart();
   console.log("=== pid jackd "+ spawnProcess1.pid)
 
-  jackd_pid = spawnProcess1.pid;
+  
 
   let i = 0;
-  let a = setInterval( () => {
+  let a = setInterval( () => {          //todo: here i will need time out 
+  console.log(" --- a = setInterval  -a")
+
     if(IsJackServerRunning()){
-      console.log("running ")
+      console.log(" --- a IsJackServerRunning == true")
+
+      //set jackd_pid because we know jackd is running
+      jackd_pid = spawnProcess1.pid;
       let spawnProcess = JackTripStart();
-      clearInterval(a); 
 
-      jacktrip_pid = spawnProcess.pid;
-      console.log("=== pid jacktrip "+ spawnProcess.pid + "  " +jacktrip_pid)
-      console.log("out of interval")
+      let b = setInterval (() => {     //todo: here i will need time out 
+        console.log(" --- b = setInterval  -a")
+        if (IsJacktripRunning()){    
+          console.log(" --- b IsJacktripRunning == true")
+
+          //set jacktrip_pid because we know jacktrip is running
+          jacktrip_pid = spawnProcess.pid; 
+          clearInterval(a);
+          clearInterval(b);
+          console.log(" --- clear intervals")
+
+          console.log("=== pid jackserver "+ spawnProcess1.pid + "  " +jackd_pid)
+          console.log("=== pid jacktrip "+ spawnProcess.pid + "  " +jacktrip_pid)
+
+          //todo create a variable and poll from outside
+        }
+        console.log(" --- b = setInterval  -x")
+      }, 300);
     }
-  }, 1000); //every one second check to see if it is runnning or not
+  }, 300); //every one second check to see if it is runnning or not
 
-  console.log(" &&&&&&&&&&&&  jacktrip pid to be killed in 10 sec: pid= " + jacktrip_pid);
-  SleepTimeoutKill(10000, jacktrip_pid);
+ // console.log(" &&&&&&&&&&&&  jacktrip pid to be killed in 10 sec: pid= " + jacktrip_pid);
+ // SleepTimeoutKill(10000, jacktrip_pid);
 
   console.log(" &&&&&&&&&&&&  jackd pid to be killed in 20 sec: pid= " + jackd_pid);
   SleepTimeoutKill(20000, jackd_pid);
@@ -353,16 +419,83 @@ function main2 (){
 console.log ("----------- jackd end ---------------x");
 }
 
+function main4 (){
+
+  killProcesses();
+  console.log(IsJacktripRunning());
+}
+
+
 // works
 function main3(){
   console.log(JacktripVersion());
   console.log(JackdVersion());
 }
 
+function main(){
+
+  console.log ("-------- begin")
+
+  let i =0;
+  let j =0;
+
+  let a = setInterval( () => {
+    console.log("a -- a" + i)
+    if(i === 3){
+      console.log("a i===3")
+      let b = setInterval( () => {
+        console.log("b " + j)
+        if(j === 5){
+          console.log("b j===5")
+          clearInterval(a);
+          clearInterval(b);
+        }
+        console.log("b incrementing j")
+        j++;
+      }, 1);
+    }
+  
+    console.log("a incrementing i")
+    i++;
+  }, 1);
+
+console.log ("-------- end")
+}
+
+
 /**---------------------------------------------------------------------------------------- */
 
+/**
+ * testing function
+ * 
+ * 1. start jacktrip
+ * 2. after 5 seconds kill jacktrip
+ * 3. after 5 seconds start jacktrip
+ */
+function main5(){
 
-//////////
+  console.log("####----- main 5 ------ start ")
+  setTimeout(function () {
+    console.log("### first -a ");
+    main2();
+    setTimeout(function () {
+      console.log("### second -a ");
 
-main3();
-main2();
+      killProcesses();
+      setTimeout(function () {
+        console.log("### thid -a ");
+
+        main2();
+        console.log("### thid -x ");
+
+      }, 5000); 
+      console.log("### second -x ");
+
+    }, 5000); 
+    console.log("### first -x ");
+  }, 5000); 
+  console.log("####----- main 5 ------ end ")
+}
+
+
+main5();
